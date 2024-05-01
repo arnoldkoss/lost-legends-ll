77bc6ed958235e4c3924da23d24855471a09218c
arnold buiuca <arnoldbuiuka@gmail.com>
2024-05-01 10:56:48 +0000

    fix the logo and font awesome and the links for better display

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.gif";
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="90" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="ml-auto text-left">
+            <Nav.Link><i class="fa-solid fa-house"></i> Home</Nav.Link>
+            <Nav.Link><i class="fa-solid fa-right-to-bracket"></i> Sign in</Nav.Link>
+            <Nav.Link><i class="fa-solid fa-user-plus"></i> Sign up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
