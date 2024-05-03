21cf614e717901f416d27e4beddca7b675418424
arnold buiuca <arnoldbuiuka@gmail.com>
2024-05-03 14:01:54 +0000

    create axios interceptors Res and Req, set axiosRes to refresh acces token if it gets401 error, set axiosReq to refresh acces token before making request

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://drfapill-6d3c36398683.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
