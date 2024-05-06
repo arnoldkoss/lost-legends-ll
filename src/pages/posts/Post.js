7cda1a619f5bdefe6b5779360e09345c37da56d8
arnold buiuca <arnoldbuiuka@gmail.com>
2024-05-06 09:47:59 +0000

    Integrate User Avatar and Name Link in Post Component

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,9 +1,49 @@
 import React from 'react'
 import styles from "../../styles/Post.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media } from 'react-bootstrap';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import Avatar from "../../components/Avatar";
 
 const Post = (props) => {
+    const {
+        id,
+        owner,
+        detectorist_id,
+        detectorist_image,
+        comments_count,
+        likes_count,
+        like_id,
+        wishlists_count,
+        wishlist_id,
+        favorites_count,
+        favorite_id,
+        title,
+        content,
+        image,
+        updated_at,
+        postPage,
+      } = props;
+
+
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+
   return (
-    <div>Post placeholder text</div>
+    <Card className={styles.Post}>
+        <Card.Body>
+            <Media className="align-items-center justify-content-between">
+                <Link to={`/detectorists/${detectorist_id}`}>
+                <Avatar src={detectorist_image} height={55} />
+                {owner}
+
+                </Link>
+
+            </Media>
+        </Card.Body>
+    </Card>
   )
 }
 
