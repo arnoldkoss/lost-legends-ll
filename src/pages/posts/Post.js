9351d669c2246c824798efc972a799b1362d9507
arnold buiuca <arnoldbuiuka@gmail.com>
2024-05-06 14:13:49 +0000

    Add asynchronous handler for liking a post with updated like count and error handling

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -4,6 +4,7 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
 
 const Post = (props) => {
     const {
@@ -14,21 +15,34 @@ const Post = (props) => {
         comments_count,
         likes_count,
         like_id,
-        wishlists_count,
-        wishlist_id,
-        favorites_count,
-        favorite_id,
         title,
         content,
         image,
         updated_at,
         postPage,
+        setPosts,
       } = props;
 
 
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+
+  const handleLike = async () => {
+    try {
+      const { data } = await axiosRes.post("/likes/", { post:id });
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+            ? { ...post, likes_count: post.likes_count + 1, like_id: data.id }
+            : post;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
 
   return (
@@ -65,7 +79,8 @@ const Post = (props) => {
               <i className={`fas fa-heart ${styles.Heart}`} />
             </span>
           ) : currentUser ? (
-            <span onClick={() => {}}>
+            <span onClick={handleLike}>
+
               <i className={`far fa-heart ${styles.HeartOutline}`} />
             </span>
           ) : (
