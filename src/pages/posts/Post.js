2a4907740fde94e6d0a397ccfc1fe4a4f2eb05d6
arnold buiuca <arnoldbuiuka@gmail.com>
2024-05-07 16:34:57 +0000

    Add conditional wishlist icons based on user authentication and wishlist status, with tooltips for login prompts and wishlist count display.

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -15,6 +15,10 @@ const Post = (props) => {
         comments_count,
         likes_count,
         like_id,
+        wishlists_count,
+        wishlist_id,
+        favorites_count,
+        favorite_id,
         title,
         content,
         image,
@@ -80,40 +84,58 @@ const Post = (props) => {
         <Card.Img src={image} alt={title} />
       </Link>
       <Card.Body>
-      {title && <Card.Title className="text-center">{title}</Card.Title>}
-        {content && <Card.Text>{content}</Card.Text>}
-        <div className={styles.PostBar}>
-        {is_owner ? (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>You can't like your own post!</Tooltip>}
-            >
-              <i className="far fa-heart" />
-            </OverlayTrigger>
-          ) : like_id ? (
-            <span onClick={handleUnlike}>
-              <i className={`fas fa-heart ${styles.Heart}`} />
-            </span>
-          ) : currentUser ? (
-            <span onClick={handleLike}>
+  {title && <Card.Title className="text-center">{title}</Card.Title>}
+  {content && <Card.Text>{content}</Card.Text>}
+  <div className={styles.PostBar}>
+    {is_owner ? (
+      <OverlayTrigger
+        placement="top"
+        overlay={<Tooltip>You can't like your own post!</Tooltip>}
+      >
+        <i className="far fa-heart" />
+      </OverlayTrigger>
+    ) : like_id ? (
+      <span onClick={handleUnlike}>
+        <i className={`fas fa-heart ${styles.Heart}`} />
+      </span>
+    ) : currentUser ? (
+      <span onClick={handleLike}>
+        <i className={`far fa-heart ${styles.HeartOutline}`} />
+      </span>
+    ) : (
+      <OverlayTrigger
+        placement="top"
+        overlay={<Tooltip>Log in to like posts!</Tooltip>}
+      >
+        <i className="far fa-heart" />
+      </OverlayTrigger>
+    )}
+    <span className={styles.LikesCount}>{likes_count}</span>
+    <Link to={`/posts/${id}`}>
+      <i className="far fa-comments" />
+    </Link>
+    <span className={styles.CommentsCount}>{comments_count}</span>
 
-              <i className={`far fa-heart ${styles.HeartOutline}`} />
-            </span>
-          ) : (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>Log in to like posts!</Tooltip>}
-            >
-              <i className="far fa-heart" />
-            </OverlayTrigger>
-          )}
-          {likes_count}
-          <Link to={`/posts/${id}`}>
-            <i className="far fa-comments" />
-          </Link>
-          {comments_count}
-        </div>
-      </Card.Body>
+    {wishlist_id ? (
+      <span onClick={() => {}}>
+        <i className={`fa-solid fa-clipboard-list ${styles.Wlist}`} />
+      </span>
+    ) : currentUser ? (
+      <span onClick={() => {}}>
+        <i className={`fa-solid fa-clipboard-list ${styles.WlistOutline}`} />
+      </span>
+    ) : (
+      <OverlayTrigger
+        placement="top"
+        overlay={<Tooltip>Log in to add posts to the Wishlist!</Tooltip>}
+      >
+        <i className="fa-solid fa-clipboard-list" />
+      </OverlayTrigger>
+      
+    )}
+    {wishlists_count}
+  </div>
+</Card.Body>
     </Card>
   )
 }
