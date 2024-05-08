044036abe29b34f330df48a7f82afd902cffbea3
arnold buiuca <arnoldbuiuka@gmail.com>
2024-05-08 09:08:41 +0000

    Add route paths for homepage, feed, and wishlist with custom messages and filters in PostsPage components.

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,4 +1,12 @@
+import React from "react";
 
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+import styles from "../../styles/PostsPage.module.css";
 
 function PostsPage() {
   
