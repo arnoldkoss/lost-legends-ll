7e0deb7ea19714e1b6646f6b4efa1947e2d41027
arnold buiuca <arnoldbuiuka@gmail.com>
2024-05-10 17:31:11 +0000

    fix the styles for the comment component

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -5,10 +5,11 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import Comment from "../comments/Comment";
+
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
@@ -19,10 +20,6 @@ function PostPage() {
   const currentUser = useCurrentUser();
   const detectorist_image = currentUser?.detectorist_image;
   const [comments, setComments] = useState({ results: [] });
-
-  console.log('currentUser: ', currentUser)
-  console.log('post: ', post)
-  console.log('comments: ', comments)
 
   useEffect(() => {
     const handleMount = async () => {
@@ -41,31 +38,32 @@ function PostPage() {
     handleMount();
   }, [id]);
 
-
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
         <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
-        {currentUser ? (
-        <CommentCreateForm
-        detectorist_id={currentUser.detectorist_id}
-        detectoristImage={detectorist_image}
-        post={id}
-        setPost={setPost}
-        setComments={setComments}
-      /> ) : comments.results.length ? (
-        "Comments"
-      ) : null} 
-      {comments.results.length ? (
-        comments.results.map(comment => (
-          <Comment key={comment.id} {...comment} />
-        ))) : currentUser ? (
-        <span>No comments yet, be the first one to write one!</span>
-      ) : (
-        <span>No comments yet!</span>
-      )}
+          {currentUser ? (
+            <CommentCreateForm
+              detectorist_id={currentUser.detectorist_id}
+              detectoristImage={detectorist_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            comments.results.map((comment) => (
+              <Comment key={comment.id} {...comment} />
+            ))
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
