e529d794beb853c27d693cd5c3ce60b3297154b7
arnold buiuca <arnoldbuiuka@gmail.com>
2024-05-11 10:31:24 +0000

    Implement data fetching for popular detectorists in PopularDetectorists component

--- src/pages/detectorists/PopularDetectorists.js
+++ src/pages/detectorists/PopularDetectorists.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import appStyles from "../../App.module.css";
 import { Container } from 'react-bootstrap';
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 
 const PopularDetectorists = () => {
+    const [detectoristData, setDetectoristData] = useState({
+        // we will use the pageProfile later!
+        pageDetectorist: { results: [] },
+        popularDetectorists: { results: [] },
+      });
+
+    const { popularDetectorists } = detectoristData;
+    const currentUser = useCurrentUser();
+    
+    useEffect(() => {
+        const handleMount = async () => {
+          try {
+            const { data } = await axiosReq.get(
+              "/detectorists/?ordering=-followers_count"
+            );
+            setDetectoristData((prevState) => ({
+              ...prevState,
+              popularDetectorists: data,
+            }));
+          } catch (err) {
+            console.log(err);
+          }
+        };
+    
+        handleMount();
+      }, [currentUser]);
+
+
   return (
     <Container className={appStyles.Content}>
         <p>Most followed detectorists</p>
+        {popularDetectorists.results.map((detectorist) => (
+            <p key={detectorist.id}>{detectorist.owner}</p>
+          ))}
     </Container>
   );
 };
