7a79744d45e973919c0f410e6668e88415516ba7
arnold buiuca <arnoldbuiuka@gmail.com>
2024-05-11 14:19:29 +0000

    Enhance Detectorist component with user links and responsive layout

--- src/pages/detectorists/Detectorist.js
+++ src/pages/detectorists/Detectorist.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from "../../styles/Detectorist.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
 
 const Detectorist = (props) => {
     const {detectorist, mobile, imageSize=55} = props;
@@ -11,7 +13,18 @@ const Detectorist = (props) => {
     const is_owner = currentUser?.username === owner;
 
   return (
-    <div>Detectorist</div>
+    <div className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    >
+        <div>
+        <Link className="align-self-center" to={`/detectorists/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
+        <strong>{owner}</strong>
+      </div>
+
+    </div>
   )
 }
 
