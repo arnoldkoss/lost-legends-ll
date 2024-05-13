baa5a5a7abab465039f083e29138dfb44b4fd171
arnold buiuca <arnoldbuiuka@gmail.com>
2024-05-13 10:22:02 +0000

    Add unfollowHelper function to adjust follower counts in Detectorist data

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -32,3 +32,22 @@ export const followHelper = (detectorist, clickedDetectorist, following_id) => {
       // the user owns, so just return it unchanged
       detectorist;
 };
+
+
+export const unfollowHelper = (detectorist, clickedDetectorist) => {
+  return detectorist.id === clickedDetectorist.id
+    ? // This is the profile I clicked on,
+      // update its followers count and set its following id
+      {
+        ...detectorist,
+        followers_count: detectorist.followers_count - 1,
+        following_id: null,
+      }
+    : detectorist.is_owner
+    ? // This is the profile of the logged in user
+      // update its following count
+      { ...detectorist, following_count: detectorist.following_count - 1 }
+    : // this is not the profile the user clicked on or the profile
+      // the user owns, so just return it unchanged
+      detectorist;
+};
